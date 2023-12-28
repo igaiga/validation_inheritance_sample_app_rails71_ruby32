ed66a811560291e391bf827b84d726ddbe7f03b7
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2023-12-28 15:08:37 +0900

    Cancel validation in child class

--- app/models/foo_user.rb
+++ app/models/foo_user.rb
@@ -1,2 +1,9 @@
 class FooUser < User
+  cancel_validates_proc = Proc.new do |attribute|
+    _validate_callbacks.
+      select{|vc| vc.filter.respond_to?(:attributes) && vc.filter.attributes == [attribute] }.
+      each{|vc| skip_callback(:validate, vc.kind, vc.filter)}
+  end
+  cancel_validates_proc.call(:nickname)
+  cancel_validates_proc.call(:note)
 end
